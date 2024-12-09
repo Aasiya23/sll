e1d55c6d238dc4b1c0045eaa70ece0d77dbbf130
Danamma11Gothe <danammagothe@gmail.com>
2024-12-09 15:12:35 +0530

    this is display commit msg

---
+++ aasiya.c
@@ -0,0 +1,19 @@
+void display()
+{
+NODE temp;
+int count=0;
+temp=first;
+if(first==NULL)
+printf("\nempty list\n");
+else
+{
+printf("\nUSN\t\tNAME\t\tBRANCH\t\tSEM\t\tPHNO");
+while(temp!=NULL)
+{
+printf("\n\n%s\t\t%s\t\t%s\t\t%d\t\t%s",temp->usn,temp->name,temp->branch,temp->sem,temp->phno);
+temp=temp->next;
+count++;
+}
+printf("\n\nthe number of nodes are %d",count);
+}
+}
